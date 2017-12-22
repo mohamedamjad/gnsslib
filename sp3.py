58c4789b08f777f1b4dbda05ae2ae5503db24d1a
Mohamed Amjad LASRI <amjad.sig@gmail.com>
2017-12-22 17:13:01 +0100

    début des développements

--- sp3.py
+++ sp3.py
@@ -3,15 +3,18 @@ import numpy as np
 import pandas as pd
 
 def read_sp3(file_path):
+    data_table = np.array([])
     df = pd.DataFrame()
     with open(file_path) as fp:
         for line in fp:
             if '*  ' in line:
                 date_time = line.split()[1:len(line.split())]
+                print(date_time)
                 line = fp.next()
                 while '*  ' not in line:
                     line = fp.next()
-                    data_table = data_table.append(date_time+line.split())
-    print(data_table)           
+                    #print(line.split())
+                    #data_table = data_table.append(date_time+line.split())
+    #print(data_table)           
 
 read_sp3('/home/anonyme/igu18222_00.sp3')
